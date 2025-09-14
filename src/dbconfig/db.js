71a775c07978b6d2d7039fe9cd870ed5c3f93f31
corpacademia <72709902+corpacademia@users.noreply.github.com>
2025-09-14 22:56:53 +0530

    Update db.js

--- src/dbconfig/db.js
+++ src/dbconfig/db.js
@@ -1,13 +1,16 @@
-const {Pool} = require('pg');
-
+const { Pool } = require('pg');
 require('dotenv').config();
 
 const pool = new Pool({
   user: process.env.USER,
   host: process.env.HOST,
-  database:process.env.DATABASE,
-  password:process.env.PASSWORD,
+  database: process.env.DATABASE,
+  password: process.env.PASSWORD,
   port: process.env.DATABASE_PORT,
+  ssl: {
+    require: true,
+    rejectUnauthorized: false, // disable CA validation for now (works with RDS)
+  },
 });
 
 module.exports = pool;
