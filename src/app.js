abeb808d706580c4e588dd4feb958e269f832bde
corpacademia <72709902+corpacademia@users.noreply.github.com>
2025-09-15 22:18:52 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -17,7 +17,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    origin: process.env.FRONTEND_URL || "httpps://app.golabing.ai",
     credentials: true,
 }));
 
